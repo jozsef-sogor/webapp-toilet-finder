0189de3bc8212f1046ebe01a65f479fe67214614
jozsef-sogor <44443711+jozsef-sogor@users.noreply.github.com>
2019-09-09 18:26:49 +0200

    Map implementation
    Google Maps API initialized
    Map put into main tab, full screen
    GeoLocation works
    (All 3 API requests used up for this month..)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,65 @@
 "use strict";
 
+
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: lat, lng: lng};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+
+
+
+//Acessing user location
+var map, infoWindow;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 15
+  });
+  infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  //Checks if the browser has access tot the user location
+  //if yes it puts the coordinates into (global) variables
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      
+      //Puts a pop-up for testing
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Gotcha...');
+      infoWindow.open(map);
+      map.setCenter(pos);
+
+
+      //centers the map to the user location
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+
+
 // hide all pages
 function hideAllPages() {
   let pages = document.querySelectorAll(".page");
