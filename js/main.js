d2a750d130dba503c781edab7d0de61dfe7446f7
jozsef-sogor <44443711+jozsef-sogor@users.noreply.github.com>
2019-09-10 13:08:21 +0200

    Uluru removed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,6 @@
 "use strict";
 
 
-function initMap() {
-  // The location of Uluru
-  var uluru = {lat: lat, lng: lng};
-  // The map, centered at Uluru
-  var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
-
-
-
 //Acessing user location
 var map, infoWindow;
 function initMap() {
@@ -31,7 +19,7 @@ function initMap() {
         lat: position.coords.latitude,
         lng: position.coords.longitude
       };
-      
+
       //Puts a pop-up for testing
       infoWindow.setPosition(pos);
       infoWindow.setContent('Gotcha...');
