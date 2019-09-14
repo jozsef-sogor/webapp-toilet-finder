96de09140700b824d2dae80234bd3abcd2f11e25
IgnasBa <cepelinas321@gmail.com>
2019-09-14 17:49:16 +0200

    hi

--- js/main.js
+++ js/main.js
@@ -253,15 +253,15 @@ var mapStyling = [{
 
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB04QXJ9nEQdJa9AWTqF_GmR8SOr_KvF7c",
-//   authDomain: "public-toilet-finder-4e2f0.firebaseapp.com",
-//   databaseURL: "https://public-toilet-finder-4e2f0.firebaseio.com",
-//   projectId: "public-toilet-finder-4e2f0",
-//   storageBucket: "",
-//   messagingSenderId: "509217784069",
-//   appId: "1:509217784069:web:3a19197f49947c53f7f76c"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyB04QXJ9nEQdJa9AWTqF_GmR8SOr_KvF7c",
+   authDomain: "public-toilet-finder-4e2f0.firebaseapp.com",
+   databaseURL: "https://public-toilet-finder-4e2f0.firebaseio.com",
+   projectId: "public-toilet-finder-4e2f0",
+   storageBucket: "",
+   messagingSenderId: "509217784069",
+   appId: "1:509217784069:web:3a19197f49947c53f7f76c"
+ };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
