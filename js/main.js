2cc1d7dc1a55b2d2a2db4623ccb8fb3d80bf5295
Eduard <edward.roman8@gmail.com>
2019-09-14 15:50:04 +0200

    one wrong elem in add page

--- js/main.js
+++ js/main.js
@@ -251,19 +251,19 @@ var mapStyling = [{
 
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB04QXJ9nEQdJa9AWTqF_GmR8SOr_KvF7c",
-  authDomain: "public-toilet-finder-4e2f0.firebaseapp.com",
-  databaseURL: "https://public-toilet-finder-4e2f0.firebaseio.com",
-  projectId: "public-toilet-finder-4e2f0",
-  storageBucket: "",
-  messagingSenderId: "509217784069",
-  appId: "1:509217784069:web:3a19197f49947c53f7f76c"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB04QXJ9nEQdJa9AWTqF_GmR8SOr_KvF7c",
+//   authDomain: "public-toilet-finder-4e2f0.firebaseapp.com",
+//   databaseURL: "https://public-toilet-finder-4e2f0.firebaseio.com",
+//   projectId: "public-toilet-finder-4e2f0",
+//   storageBucket: "",
+//   messagingSenderId: "509217784069",
+//   appId: "1:509217784069:web:3a19197f49947c53f7f76c"
+// };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-const db = firebase.firestore();
+// firebase.initializeApp(firebaseConfig);
+//
+// const db = firebase.firestore();
 const bathroomRef = db.collection("locations");
 
 
@@ -333,9 +333,9 @@ function initMap() {
   });
   infoWindow = new google.maps.InfoWindow;
   //loading geoJSON data
-  map.data.loadGeoJson('locations.json');
-  map.data.addGeoJson();
-  map.data.setMap(map);
+  // map.data.loadGeoJson('locations.json');
+  // map.data.addGeoJson();
+  // map.data.setMap(map);
 
   // Try HTML5 geolocation.
   //Checks if the browser has access tot the user location
